977105d40719b54875866faa29d897b0785e65c8
deadmarston <deadmarston@163.com>
2019-11-27 16:12:26 +0900

    the origin pbrt v3 code

---
+++ src/ext/glog/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/deadmarston/Workspace/pbrt-v3/src/ext/glog
+# Build directory: /home/deadmarston/Workspace/pbrt-v3/src/ext/glog
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(demangle "/home/deadmarston/Workspace/pbrt-v3/src/ext/glog/demangle_unittest")
+add_test(logging "/home/deadmarston/Workspace/pbrt-v3/src/ext/glog/logging_unittest")
+add_test(signalhandler "/home/deadmarston/Workspace/pbrt-v3/src/ext/glog/signalhandler_unittest")
+add_test(stacktrace "/home/deadmarston/Workspace/pbrt-v3/src/ext/glog/stacktrace_unittest")
+add_test(stl_logging "/home/deadmarston/Workspace/pbrt-v3/src/ext/glog/stl_logging_unittest")
+add_test(symbolize "/home/deadmarston/Workspace/pbrt-v3/src/ext/glog/symbolize_unittest")
---
+++ src/ext/ptex/src/ptex/CMakeFiles/Ptex_static.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/deadmarston/Workspace/pbrt-v3/src/ext/ptex/src/ptex/PtexCache.cpp" "/home/deadmarston/Workspace/pbrt-v3/src/ext/ptex/src/ptex/CMakeFiles/Ptex_static.dir/PtexCache.cpp.o"
+  "/home/deadmarston/Workspace/pbrt-v3/src/ext/ptex/src/ptex/PtexFilters.cpp" "/home/deadmarston/Workspace/pbrt-v3/src/ext/ptex/src/ptex/CMakeFiles/Ptex_static.dir/PtexFilters.cpp.o"
+  "/home/deadmarston/Workspace/pbrt-v3/src/ext/ptex/src/ptex/PtexHalf.cpp" "/home/deadmarston/Workspace/pbrt-v3/src/ext/ptex/src/ptex/CMakeFiles/Ptex_static.dir/PtexHalf.cpp.o"
+  "/home/deadmarston/Workspace/pbrt-v3/src/ext/ptex/src/ptex/PtexReader.cpp" "/home/deadmarston/Workspace/pbrt-v3/src/ext/ptex/src/ptex/CMakeFiles/Ptex_static.dir/PtexReader.cpp.o"
+  "/home/deadmarston/Workspace/pbrt-v3/src/ext/ptex/src/ptex/PtexSeparableFilter.cpp" "/home/deadmarston/Workspace/pbrt-v3/src/ext/ptex/src/ptex/CMakeFiles/Ptex_static.dir/PtexSeparableFilter.cpp.o"
+  "/home/deadmarston/Workspace/pbrt-v3/src/ext/ptex/src/ptex/PtexSeparableKernel.cpp" "/home/deadmarston/Workspace/pbrt-v3/src/ext/ptex/src/ptex/CMakeFiles/Ptex_static.dir/PtexSeparableKernel.cpp.o"
+  "/home/deadmarston/Workspace/pbrt-v3/src/ext/ptex/src/ptex/PtexTriangleFilter.cpp" "/home/deadmarston/Workspace/pbrt-v3/src/ext/ptex/src/ptex/CMakeFiles/Ptex_static.dir/PtexTriangleFilter.cpp.o"
+  "/home/deadmarston/Workspace/pbrt-v3/src/ext/ptex/src/ptex/PtexTriangleKernel.cpp" "/home/deadmarston/Workspace/pbrt-v3/src/ext/ptex/src/ptex/CMakeFiles/Ptex_static.dir/PtexTriangleKernel.cpp.o"
+  "/home/deadmarston/Workspace/pbrt-v3/src/ext/ptex/src/ptex/PtexUtils.cpp" "/home/deadmarston/Workspace/pbrt-v3/src/ext/ptex/src/ptex/CMakeFiles/Ptex_static.dir/PtexUtils.cpp.o"
+  "/home/deadmarston/Workspace/pbrt-v3/src/ext/ptex/src/ptex/PtexWriter.cpp" "/home/deadmarston/Workspace/pbrt-v3/src/ext/ptex/src/ptex/CMakeFiles/Ptex_static.dir/PtexWriter.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "src/ext/openexr/IlmBase/Imath"
+  "src/ext/openexr/IlmBase/Half"
+  "src/ext/openexr/IlmBase/Iex"
+  "src/ext/openexr/OpenEXR/IlmImf"
+  "src/ext/openexr/IlmBase/config"
+  "src/ext/openexr/OpenEXR/config"
+  "src/ext/glog/src"
+  "src/ext/glog"
+  "src/ext/ptex/src/ptex"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
